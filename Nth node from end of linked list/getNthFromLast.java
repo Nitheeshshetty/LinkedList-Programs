18a651686a3eef5227a5ee6094cc05419db98124
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-03-21 17:12:26 +0530

    Update getNthFromLast.java

--- Nth node from end of linked list/getNthFromLast.java
+++ Nth node from end of linked list/getNthFromLast.java
@@ -9,14 +9,11 @@ class Node
         next = null;
     }
 }
-	
-
-	
+		
 public class LinkedList_Element_From_Last
 {
     Node head;  
-	Node tail;
-	/* Function to print linked list */
+    Node tail;
     void printList(Node head)
     {
         Node temp = head;
@@ -31,44 +28,40 @@ public class LinkedList_Element_From_Last
  
     /* Inserts a new Node at front of the list. */
     public void addToTheLast(Node node) 
+    {
+	if (head == null) 
 	{
-
-		if (head == null) 
-		{
-			head = node;
-			tail = node;
-		} else 
-		{
-		    tail.next = node;
-		    tail = node;
-		}
+	    head = node;
+	    tail = node;
+	}
+	else {
+	    tail.next = node;
+	    tail = node;
+	}
     }
     public static void main(String args[])
     {
-         Scanner sc = new Scanner(System.in);
-		 int t=sc.nextInt();
-		 
-		 while(t>0)
-     {
-			int n = sc.nextInt();
-			int k = sc.nextInt();
-			LinkedList_Element_From_Last llist = new LinkedList_Element_From_Last();
-			//int n=Integer.parseInt(br.readLine());
-			int a1=sc.nextInt();
-			Node head= new Node(a1);
-            llist.addToTheLast(head);
-            for (int i = 1; i < n; i++) 
-			{
-				int a = sc.nextInt(); 
-				llist.addToTheLast(new Node(a));
-			}
-        GfG g = new GfG(); 
-		//System.out.println(k);
-		System.out.println(g.getNthFromLast(llist.head,k));
-		
-			t--;
-		}
+        Scanner sc = new Scanner(System.in);
+	int t=sc.nextInt();
+	while(t>0)
+     	{
+	     int n = sc.nextInt();
+	     int k = sc.nextInt();
+	     LinkedList_Element_From_Last llist = new LinkedList_Element_From_Last();
+	     int a1=sc.nextInt();
+	     Node head= new Node(a1);
+             llist.addToTheLast(head);
+             for (int i = 1; i < n; i++) 
+	     {
+	  	 int a = sc.nextInt(); 
+		 llist.addToTheLast(new Node(a));
+	     }
+             GfG g = new GfG(); 
+	     //System.out.println(k);
+	     System.out.println(g.getNthFromLast(llist.head,k));
+	     t--;
 	}
+    }
 }
 
 
