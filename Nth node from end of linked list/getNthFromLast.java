7c47b2df87bdbf357e207b4a9d8278d92bb74cb2
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-03-21 17:07:25 +0530

    Create getNthFromLast.java

---
+++ Nth node from end of linked list/getNthFromLast.java
@@ -0,0 +1,97 @@
+import java.util.*;
+class Node
+{
+    int data;
+    Node next;
+    Node(int d) 
+    {
+        data = d; 
+        next = null;
+    }
+}
+	
+
+	
+public class LinkedList_Element_From_Last
+{
+    Node head;  
+	Node tail;
+	/* Function to print linked list */
+    void printList(Node head)
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+           System.out.print(temp.data+" ");
+           temp = temp.next;
+        }  
+        System.out.println();
+    }
+	
+ 
+    /* Inserts a new Node at front of the list. */
+    public void addToTheLast(Node node) 
+	{
+
+		if (head == null) 
+		{
+			head = node;
+			tail = node;
+		} else 
+		{
+		    tail.next = node;
+		    tail = node;
+		}
+    }
+    public static void main(String args[])
+    {
+         Scanner sc = new Scanner(System.in);
+		 int t=sc.nextInt();
+		 
+		 while(t>0)
+     {
+			int n = sc.nextInt();
+			int k = sc.nextInt();
+			LinkedList_Element_From_Last llist = new LinkedList_Element_From_Last();
+			//int n=Integer.parseInt(br.readLine());
+			int a1=sc.nextInt();
+			Node head= new Node(a1);
+            llist.addToTheLast(head);
+            for (int i = 1; i < n; i++) 
+			{
+				int a = sc.nextInt(); 
+				llist.addToTheLast(new Node(a));
+			}
+        GfG g = new GfG(); 
+		//System.out.println(k);
+		System.out.println(g.getNthFromLast(llist.head,k));
+		
+			t--;
+		}
+	}
+}
+
+
+class GfG
+{
+    int getNthFromLast(Node head, int n)
+    {
+    	Stack<Node>stack =new Stack<>();
+        Node temp=head;
+        if(head==null)
+            return -1;
+        while(temp!=null)
+        {
+            stack.push(temp);
+            temp=temp.next;
+        }
+        if(stack.size()<n)
+            return -1;
+        while(n-1>0)
+        {
+            stack.pop();
+            n--;
+        }
+        return stack.peek().data;	
+    }
+}
