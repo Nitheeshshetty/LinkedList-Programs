4a5b7c4cb349d919c1901cd4c5bf9c0e024deb0b
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-03-22 18:05:18 +0530

    Update insertInMid.java

--- Insert in Middle of Linked List/insertInMid.java
+++ Insert in Middle of Linked List/insertInMid.java
@@ -1,4 +1,3 @@
-//{ Driver Code Starts
 import java.util.*;
 import java.io.*;
 
@@ -16,19 +15,15 @@ public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner (System.in);
         int t = sc.nextInt();
-        
         while(t-->0){
             int n = sc.nextInt();   
             Node head = new Node(sc.nextInt());
             Node tail = head;
-            
             for(int i=0; i<n-1; i++){
                 tail.next = new Node(sc.nextInt());
                 tail = tail.next;
             }
             int key = sc.nextInt();
-            
-            //display(head);
             Solution obj = new Solution();
             obj.insertInMid(head, key);
             display(head);
@@ -45,21 +40,8 @@ public class Main {
     }
 }
 
-// } Driver Code Ends
 
 
-/*
-Structure of node class is:
-class Node {
-    int data;
-    Node next;
-    
-    public Node(int data){
-        this.data = data;
-        this.next = null;
-    }
-}
-*/
 class Solution 
 {
     public Node insertInMid(Node head, int data)
