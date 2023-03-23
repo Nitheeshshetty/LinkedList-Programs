5ad067211e6e4248ed39f3eac167872758aa5b90
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-03-23 18:48:52 +0530

    Update isLengthEvenorOdd.java

--- Linked List Length Even or Odd?/isLengthEvenorOdd.java
+++ Linked List Length Even or Odd?/isLengthEvenorOdd.java
@@ -26,26 +26,25 @@ public class GFG2
     public static void main(String args[])
     {
          Scanner sc = new Scanner(System.in);
-		 int t=sc.nextInt();
-		 
-		 while(t-->0)
+	 int t=sc.nextInt();	 
+	 while(t-->0)
          {
-			int n = sc.nextInt();
-            Node head = new Node(sc.nextInt());
-            Node tail = head;
-            for(int i=0; i<n-1; i++)
-            {
-                tail.next = new Node(sc.nextInt());
-                tail = tail.next;
-            }
-		    GFG obj=new GFG();
-			int start=obj.isLengthEvenorOdd(head);
-		    if(start==0)
-		    System.out.println("Even");
-		    else
-		    System.out.println("Odd");
-		}
+	     int n = sc.nextInt();
+             Node head = new Node(sc.nextInt());
+             Node tail = head;
+             for(int i=0; i<n-1; i++)
+             {
+                  tail.next = new Node(sc.nextInt());
+                  tail = tail.next;
+             }
+	     GFG obj=new GFG();
+	     int start=obj.isLengthEvenorOdd(head);
+	     if(start==0)
+		 System.out.println("Even");
+	     else
+		 System.out.println("Odd");
 	}
+    }
 }
 
 
