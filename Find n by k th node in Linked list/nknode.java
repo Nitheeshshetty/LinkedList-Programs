798944d8318c6eb3f50f9f2a7c5e398299bdb3c2
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-03-24 18:12:00 +0530

    Update nknode.java

--- Find n by k th node in Linked list/nknode.java
+++ Find n by k th node in Linked list/nknode.java
@@ -15,40 +15,41 @@ class Node
 class LinkedList
 {
     static Node head;
-     public static void addToTheLast(Node node) 
+    public static void addToTheLast(Node node) 
+    {
+	if (head == null) 
 	{
-		if (head == null) 
-		{
-			head = node;
-		} else 
-		{
-		   Node temp = head;
-		   while (temp.next != null)
-		   temp = temp.next;
-		   temp.next = node;
-		}
+	    head = node;
+	}
+	else 
+	{
+	    Node temp = head;
+	    while (temp.next != null)
+		temp = temp.next;
+	    temp.next = node;
+	 }
     }
     
-     public static void main (String[] args) {
+     public static void main (String[] args) 
+     {
          Scanner sc = new Scanner(System.in);
-		 int t=sc.nextInt();
-		 
-		 while(t-- > 0)
-		 {
-		     int n = sc.nextInt();
-		     int a1 = sc.nextInt();
-		     Node head = new Node(a1);
-		     addToTheLast(head);
-		     for(int i = 1; i < n; i++)
-		     {
-		         int a = sc.nextInt(); 
-				 addToTheLast(new Node(a));
-			 }
-			 int k = sc.nextInt();
-			GfG gfg = new GfG();
-			System.out.println(gfg.nknode(head, k));
-		     }
-		 }
+	 int t=sc.nextInt();
+	 while(t-- > 0)
+	 {
+	      int n = sc.nextInt();
+	      int a1 = sc.nextInt();
+	      Node head = new Node(a1);
+	      addToTheLast(head);
+	      for(int i = 1; i < n; i++)
+	      {
+		   int a = sc.nextInt(); 
+		   addToTheLast(new Node(a));
+	      }
+	      int k = sc.nextInt();
+	      GfG gfg = new GfG();
+	      System.out.println(gfg.nknode(head, k));
+	  }
+     }
 }
 
 
