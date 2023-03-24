b40cc18f77bf808f9a2362b1cc36358086b82540
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-03-24 18:05:25 +0530

    Create nknode.java

---
+++ Find n by k th node in Linked list/nknode.java
@@ -0,0 +1,95 @@
+// { Driver Code Starts
+import java.util.*;
+
+class Node
+{
+    int data;
+    Node next;
+    
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+class LinkedList
+{
+    static Node head;
+    
+     public static void addToTheLast(Node node) 
+	{
+		if (head == null) 
+		{
+			head = node;
+		} else 
+		{
+		   Node temp = head;
+		   while (temp.next != null)
+		   temp = temp.next;
+		   temp.next = node;
+		}
+    }
+    
+     public static void main (String[] args) {
+         Scanner sc = new Scanner(System.in);
+		 int t=sc.nextInt();
+		 
+		 while(t-- > 0)
+		 {
+		     int n = sc.nextInt();
+		     int a1 = sc.nextInt();
+		     Node head = new Node(a1);
+		     addToTheLast(head);
+		     for(int i = 1; i < n; i++)
+		     {
+		         int a = sc.nextInt(); 
+				 addToTheLast(new Node(a));
+			 }
+			 int k = sc.nextInt();
+			GfG gfg = new GfG();
+			System.out.println(gfg.nknode(head, k));
+		     }
+		 }
+}// } Driver Code Ends
+
+
+/*
+class Node
+{
+    int data;
+    Node next;
+    
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+*/
+
+class GfG
+{
+    public static int nknode(Node head, int k)
+    {
+        Node p1=head;
+       Node p2=head;
+       
+       int p1Pos=1;
+       int p2Pos=1;
+       
+       while(p1.next!=null){
+           p1=p1.next;
+           p1Pos++;
+           
+           int reqPos=(int)Math.ceil((1.0*p1Pos)/k);
+           
+           while(p2Pos<reqPos){
+               p2=p2.next;
+               p2Pos++;
+           }
+       }
+       
+       return p2.data;
+    }
+}
