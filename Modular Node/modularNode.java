2d66ac133cbba2866653c2cf796098a3f75d3f8b
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-03-25 17:12:18 +0530

    Update modularNode.java

--- Modular Node/modularNode.java
+++ Modular Node/modularNode.java
@@ -50,20 +50,7 @@ class Modular
     }
 }
 
-// } Driver Code Ends
 
-
-/*node class of the linked list is as:
-class Node {
-	int data;
-     Node next;
-     Node(int key)
-     {
-         data = key;
-         next = null;
-     }
-}
-*/
 // Your task is to complete the function
 // function should return the modular Node
 // if no such node is present then return -1
