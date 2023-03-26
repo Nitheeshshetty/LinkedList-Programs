476e7015001f145354f79fb474efa577ad06a89d
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-03-26 19:07:08 +0530

    Update removeDuplicates.java

--- Remove duplicate element from sorted Linked List/removeDuplicates.java
+++ Remove duplicate element from sorted Linked List/removeDuplicates.java
@@ -1,30 +1,29 @@
-// { Driver Code Starts
-/* package whatever; // don't place package name! */
 import java.util.*;
 class Node
-    {
-        int data;
-        Node next;
-        Node(int d) {data = d; next = null; }
-    }
+{
+    int data;
+    Node next;
+    Node(int d) {data = d; next = null; }
+}
+
 class Remove_Duplicate_From_LL
 {
     Node head;  
     Node tail;
-	public void addToTheLast(Node node) 
+    public void addToTheLast(Node node) 
+    {
+	if (head == null) 
 	{
-	  if (head == null) 
-	  {
-	   head = node;
-	   tail = node;
-	  } 
-	  else 
-	  {
-	   tail.next = node;
-	   tail = node;
-	  }
-	}
-      void printList()
+	    head = node;
+	    tail = node;
+	} 
+	else 
+	{
+	    tail.next = node;
+	    tail = node;
+	 }
+    }
+    void printList()
     {
         Node temp = head;
         while (temp != null)
@@ -34,31 +33,30 @@ class Remove_Duplicate_From_LL
         }  
         System.out.println();
     }
-	/* Drier program to test above functions */
-	public static void main(String args[])
+	
+    public static void main(String args[])
     {
-         Scanner sc = new Scanner(System.in);
-		 int t=sc.nextInt();
-		 while(t>0)
-         {
-			int n = sc.nextInt();
-			Remove_Duplicate_From_LL llist = new Remove_Duplicate_From_LL(); 
-			int a1=sc.nextInt();
-			Node head= new Node(a1);
+        Scanner sc = new Scanner(System.in);
+	int t=sc.nextInt();
+	while(t>0)
+        {
+	    int n = sc.nextInt();
+	    Remove_Duplicate_From_LL llist = new Remove_Duplicate_From_LL(); 
+	    int a1=sc.nextInt();
+	    Node head= new Node(a1);
             llist.addToTheLast(head);
             for (int i = 1; i < n; i++) 
-			{
-				int a = sc.nextInt(); 
-				llist.addToTheLast(new Node(a));
-			}
-			
-        GfG g = new GfG();
-		llist.head = g.removeDuplicates(llist.head);
-		llist.printList();
-		
-        t--;		
+	    {
+		int a = sc.nextInt(); 
+		llist.addToTheLast(new Node(a));
+	    }		
+            GfG g = new GfG();
+	    llist.head = g.removeDuplicates(llist.head);
+	    llist.printList();
+            t--;		
         }
-    }}// } Driver Code Ends
+    }
+}
 
 
 /*
