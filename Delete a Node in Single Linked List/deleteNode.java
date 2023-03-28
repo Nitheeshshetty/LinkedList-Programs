d07f8bbed736690c713491e25141d4351b9bb1e7
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-03-28 17:10:45 +0530

    Update deleteNode.java

--- Delete a Node in Single Linked List/deleteNode.java
+++ Delete a Node in Single Linked List/deleteNode.java
@@ -68,22 +68,9 @@ class DeleteNode
 	}
     }
 }
-// } Driver Code Ends
 
 
-/* Linklist node structure
-class Node
-{
-	int data;
-	Node next;
-	
-	Node(int d)
-	{
-		data = d;
-		next = null;
-	}
-}*/
-/*You are required to complete below method*/
+
 class GfG
 {
     Node deleteNode(Node head, int x)
