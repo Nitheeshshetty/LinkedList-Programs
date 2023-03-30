30860b26cb3f7000d7400d12e6f58d30ed4e84a5
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-03-30 16:53:14 +0530

    Update moveToFront.java

--- Move Last Element to Front of a Linked List/moveToFront.java
+++ Move Last Element to Front of a Linked List/moveToFront.java
@@ -57,24 +57,6 @@ class GFG {
 
 
 
-/*
-
-Definition for singly Link List Node
-class Node
-{
-    int data;
-    Node next;
-    
-    Node(int x){
-        data = x;
-        next = null;
-    }
-}
-
-You can also use the following for printing the link list.
-Node.printList(Node node);
-*/
-
 class Solution {
     public static Node moveToFront(Node head) {
         if(head==null || head.next==null)
