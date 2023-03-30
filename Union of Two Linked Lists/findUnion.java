8c0b04cd8628744d9b3e39c097caa10ee0130bef
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-03-30 16:43:20 +0530

    Update findUnion.java

--- Union of Two Linked Lists/findUnion.java
+++ Union of Two Linked Lists/findUnion.java
@@ -7,34 +7,28 @@ class Node
     Node next;
     
     Node(int a)
-        {
-            data = a;
-            next = null;
-        }
+    {
+       data = a;
+       next = null;
+    }
 }
 
 class GfG
 {
     static Scanner sc = new Scanner(System.in);
-    
     public static Node inputList(int size)
     {
         Node head, tail;
-        int val;
-        
+        int val; 
         val = sc.nextInt();
-        
         head = tail = new Node(val);
-        
         size--;
-        
         while(size-->0)
         {
             val = sc.nextInt();
             tail.next = new Node(val);
             tail = tail.next;
         }
-        
         return head;
     }
     
