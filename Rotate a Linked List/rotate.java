3851590f75989c9ba8558f99e2508f9a9e3bcdab
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-04-05 18:57:58 +0530

    Update rotate.java

--- Rotate a Linked List/rotate.java
+++ Rotate a Linked List/rotate.java
@@ -12,11 +12,10 @@ class Node
 
 class Main 
 {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) 
+    {    
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-        
         while (t-- > 0) {
             int n = sc.nextInt();
             
