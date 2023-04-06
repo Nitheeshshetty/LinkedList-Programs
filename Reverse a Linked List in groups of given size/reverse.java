2f3ec49b9b9911e93d9783a91af3392b635eadd8
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-04-06 21:41:41 +0530

    Update reverse.java

--- Reverse a Linked List in groups of given size/reverse.java
+++ Reverse a Linked List in groups of given size/reverse.java
@@ -54,20 +54,6 @@ class ReverseInSize
 // } Driver Code Ends
 
 
-/*node class of the linked list
-
-class Node
-{
-    int data;
-    Node next;
-    Node(int key)
-    {
-        data = key;
-        next = null;
-    }
-}
-
-*/
 
 class Solution
 {
