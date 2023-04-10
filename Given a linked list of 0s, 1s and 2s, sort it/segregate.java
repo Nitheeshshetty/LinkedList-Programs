5cefc07194f5a972692600a9d434259659d687e0
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-04-10 18:47:32 +0530

    Update segregate.java

--- Given a linked list of 0s, 1s and 2s, sort it/segregate.java
+++ Given a linked list of 0s, 1s and 2s, sort it/segregate.java
@@ -39,16 +39,13 @@ class Driverclass
     {
         if(head == null)
            return;
-           
         Node temp = head;
         while(temp != null)
         {
             System.out.print(temp.data + " ");
             temp = temp.next;
         }
-    }
-    
-    
+    }   
 }
 
 
