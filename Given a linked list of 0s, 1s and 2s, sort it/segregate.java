1f86f20f013066159b4bcfba20f3b57afb06b0ee
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-04-10 18:47:46 +0530

    Update segregate.java

--- Given a linked list of 0s, 1s and 2s, sort it/segregate.java
+++ Given a linked list of 0s, 1s and 2s, sort it/segregate.java
@@ -56,18 +56,7 @@ class Driverclass
 
 //User function Template for Java
 
-/*
-class Node
-{
-    int data;
-    Node next;
-    Node(int data)
-    {
-        this.data = data;
-        next = null;
-    }
-}
-*/
+
 class Solution
 {
     //Function to sort a linked list of 0s, 1s and 2s.
