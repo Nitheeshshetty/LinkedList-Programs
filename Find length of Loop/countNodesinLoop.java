332d987eab61ee37e617ff8a222678a334895b18
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-04-11 13:33:00 +0530

    Update countNodesinLoop.java

--- Find length of Loop/countNodesinLoop.java
+++ Find length of Loop/countNodesinLoop.java
@@ -26,28 +26,24 @@ class GFG
         tail.next = curr;
     }
     
-    public static void main (String[] args){
+    public static void main (String[] args)
+    {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-        
         while(t--> 0)
         {
             int n = sc.nextInt();
-            
             int num = sc.nextInt();
             Node head = new Node(num);
             Node tail = head;
-            
             for(int i=0; i<n-1; i++)
             {
                 num = sc.nextInt();
                 tail.next = new Node(num);
                 tail = tail.next;
             }
-            
             int pos = sc.nextInt();
             makeLoop(head, tail, pos);
-            
             Solution x = new Solution();
             System.out.println( x.countNodesinLoop(head) );
         }
